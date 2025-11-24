728fa32112651ab4d9d6da3ba4cc2c03722d3056
Iure Castro <55067724+iurecastro@users.noreply.github.com>
2025-11-24 15:06:43 +0000

    Create Implementation1.js
    Storing Username in Cookie (Insecure)

---
+++ Implementation1.js
@@ -0,0 +1,33 @@
+//Storing Username in Cookie (Insecure)
+
+// Server-side user database
+const USERS = {
+  alice: 'password123',
+  bob: 'hunter2'
+};
+
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+  if (password === USERS[username]) {
+    res.cookie('username', username);
+    res.redirect('/');
+  } else {
+    res.send('Login failed!');
+  }
+});
+
+app.get('/', (req, res) => {
+  const { username } = req.cookies;
+  if (username) {
+    res.send(`Welcome ${username}!`);
+  } else {
+    res.send(loginForm);
+  }
+});
+
+
+//Log in as 'alice'
+
+//Edit cookie to say 'username=bob'
+
+//Now you're logged in as Bob!
